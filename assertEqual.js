3f4ed5ad701b14126b309ea3294de1a4d1db0e5d
Idil Mahamed <mahamed.idil@gmail.com>
2023-08-28 01:56:30 -0400

    Added assertequal

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log("✅✅✅  Assertion Passed:" + actual + "===" + expected);
+
+  }
+  if (actual !== expected) {
+    console.log(`🟥🟥🟥 Assertion Failed:${actual} !== ${expected}`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
