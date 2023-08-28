cae64e554772b577ef5f7d04a3ee0d1efbb5fbab
Idil Mahamed <mahamed.idil@gmail.com>
2023-08-28 03:26:30 -0400

    Implenting tail

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
+
+  }
+  if (actual !== expected) {
+    console.log(`🟥🟥🟥 Assertion Failed:${actual} !== ${expected}`);
+  }
+};
+
+
+
+const tail = function (arr) {
+  return arr.slice(1)
+}
+
+assertEqual(tail([5, 6, 7]).length, 2)
+
+assertEqual(tail([5, 6, 7])[0], 6)
+
+assertEqual(tail([5, 6, 7])[1], 7)
+
+
