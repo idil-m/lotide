98e3030c82df890daa4ebacbe9dcfbb7c0f8c258
Idil Mahamed <mahamed.idil@gmail.com>
2023-09-17 01:09:03 -0400

    Refactor with Mocha and Chai- middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,42 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe("#middle", () => {
+  it("returns 1 element from odd length array big case", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]),[3]);
+ 
+  });
+  it("returns 1 element from odd length array small case", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]); 
+  });
+  it("returns empty array for middle(['']", () => {
+    assert.deepEqual(middle([]),[]); 
+  });
+  it("returns 2 element from even length array closest to the middle small case", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+  it("returns 2 element from even length array closest to the middle big case", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("is a single digist odd length array returns an empty array for middle(['']", () => {
+    assert.deepEqual(middle([1]),[]); 
+  });
+  it("is a double digist even length array returns an empty array for middle(['']", () => {
+    assert.deepEqual(middle([1,2]),[]); 
+  });
+});
 
-//no middle cases
+/*//no middle cases
 console.log(middle([]),[])
 console.log(middle([1]),[])
 console.log(middle([1,2]),[])
 
 //odd
-console.log(middle([1, 2, 3]), [2])
-console.log(middle([1, 2, 3, 4, 5]),[3])
+console.log(middle([1, 2, 3]), [2]) "returns 1 element from odd length array small case"
+console.log(middle([1, 2, 3, 4, 5]),[3]) "returns 1 element from odd length array big case"
 
 
 //even
 console.log(middle([1, 2, 3, 4]), [2, 3])
 console.log(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+*/
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,25 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+  it("returns a length of 2 for [5, 6, 7]", () => {
+    assert.strictEqual(tail([5, 6, 7]).length, 2);
+ 
+  });
+  it("returns 6 for tail([5, 6, 7])[0]", () => {
+    assert.strictEqual(tail([5, 6, 7])[0], 6); 
+  });
 
-assertEqual(tail([5, 6, 7]).length, 2)
+  it("returns 7 for tail([5, 6, 7])[1]", () => {
+  assert.strictEqual(tail([5, 6, 7])[1], 7); 
+});
+});
 
-assertEqual(tail([5, 6, 7])[0], 6)
 
-assertEqual(tail([5, 6, 7])[1], 7)
+
+
+//assertEqual(tail([5, 6, 7]).length, 2)
+
+//assertEqual(tail([5, 6, 7])[0], 6)
+
+//assertEqual(tail([5, 6, 7])[1], 7)
