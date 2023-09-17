e3c89e28128c5f458f0038ce5d5d36d9859f210f
Idil Mahamed <mahamed.idil@gmail.com>
2023-09-16 22:14:55 -0400

    tail.js refractor

---
+++ test/tailTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+
+assertEqual(tail([5, 6, 7]).length, 2)
+
+assertEqual(tail([5, 6, 7])[0], 6)
+
+assertEqual(tail([5, 6, 7])[1], 7)
